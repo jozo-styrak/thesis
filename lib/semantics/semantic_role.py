f24ab3f721ee9929389dcb76a59747be8e9b444f
Jozef Štyrák <jozef.styrak@gmail.com>
2014-03-16 12:17:45 +0100

    test

--- lib/semantics/semantic_role.py
+++ lib/semantics/semantic_role.py
@@ -1,7 +1,6 @@
 # class representing semantic role which translates to verbalex roles
 class SemanticRole:
 
-
     def __init__(self, first_lvl_role, second_lvl_role):
         self.first_level_role = first_lvl_role
         self.second_level_role = second_lvl_role
