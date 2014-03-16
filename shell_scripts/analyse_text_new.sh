48755cd1214041318638dbe3434c4ddc1a16bcc7
Jozef Štyrák <jozef.styrak@gmail.com>
2014-03-16 23:05:42 +0100

    small changes

--- shell_scripts/analyse_text_new.sh
+++ shell_scripts/analyse_text_new.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # newer version of analyser script, which runs code from analyse_text.py
-set_output = "$(mktemp)"
+set_output="$(mktemp)"
 
 /home/xstyrak/thesis/shell_scripts/process_text.sh $1 |\
 /home/xstyrak/thesis/shell_scripts/set.sh > $set_output
