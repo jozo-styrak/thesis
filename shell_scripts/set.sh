dbab2c76769fb71840a38c38ad6393843f87c497
Jozef Styrak <xstyrak@aurora.fi.muni.cz>
2014-03-16 23:16:19 +0100

    removed errors

--- shell_scripts/set.sh
+++ shell_scripts/set.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # set script containing syntactic preprocessor
-python /home/xstyrak/thesis/shell_scripts/syntactic_preprocessor_02.py |\
+python /home/xstyrak/thesis/syntactic_preprocessor_02.py |\
 /nlp/projekty/set/set/set.py --marx 2>/dev/null 
