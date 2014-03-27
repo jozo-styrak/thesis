e85ef27b2c58a90a44c14fbaca71aebcc0c49cd7
Jozef Štyrák <jozef.styrak@gmail.com>
2014-03-28 00:11:38 +0100

    update

--- lib/POS_editor.py
+++ lib/POS_editor.py
@@ -18,7 +18,7 @@ import re
 REAL_NUMBER_PATTERN = re.compile('[\+-]*\d+[\.,/:]*\d*')
 
 # lists of words used for tag editing
-NUM_FOLLOW = ['KČ', 'Kč', '%', 'euro', 'USD', 'mil', 'tis', 'PLN', 'NOK', 'HUN', 'GBP', 'AUD', 'JPY', 'CHF', 'RUB', 'eur', 'dolar']
+NUM_FOLLOW = ['KČ', 'Kč', '%', 'euro', 'USD', 'mil', 'tis', 'PLN', 'NOK', 'HUN', 'GBP', 'AUD', 'JPY', 'CHF', 'RUB', 'eur', 'dolar', 'jen']
 RECOMMENDATIONS = ['držet', 'koupit', 'kupovat', 'redukovat', 'akumulovat', 'prodat', 'strong', 'buy', 'hold', 'sell', 'neutral', 'market', 'perform', 'underperform', 'underweight', 'accumulate', 'outperform', 'swap', 'overweight', 'reduce', 'equalweight']
 RECOMMENDATION_SYNONYMS = ['doporučení', 'titul', 'předchozí', 'stupeň']
 AGENCIES = ['Goldman_Sachs', 'Morgan_Stanley', 'Credit_Suisse', 'Erste_Group']
