a36207a69a3306f8c31d265132f3ebdc48a519d8
Jozef Štyrák <jozef.styrak@gmail.com>
2014-05-15 01:02:59 +0200

    refactoring update

--- ner_script.py
+++ ner_script.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python
-# pipeline script for running NER
+# script for running NER
 
 import sys
 
 from lib.pipeline_utils import PipelineUtils
 from lib.ner.ner import executeNER, changePOSTags
-
 
 if len(sys.argv) > 1:
     # for development purposes on my PC
