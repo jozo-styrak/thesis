39f172e30388447c24d208951a20bc4d167afbaa
Jozef Štyrák <jozef.styrak@gmail.com>
2014-05-15 01:07:01 +0200

    refactoring update

--- extract_recommendations.sh
+++ extract_recommendations.sh
@@ -7,6 +7,6 @@ set_output="$(mktemp)"
 #/home/xstyrak/thesis/extract_recommendations.py $set_output
 
 ./tag_file.sh $1 |\
-./pipeline_scripts/ner_script.py |\
+./ner_script.py |\
 ./set.sh > $set_output
 ./extract_recommendations.py $set_output
